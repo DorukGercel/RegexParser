63f2d3dbccbf838e2086261c3621292b75bf8e38
DorukGercel <dorukgercel@hotmail.com>
2023-02-18 23:06:15 +0300

    add pattern matcher

---
+++ src/main/kotlin/PatternMatcher.kt
@@ -0,0 +1,32 @@
+class PatternMatcher {
+    companion object {
+        private fun addNextState(state: State, nextStates: MutableList<State>, visited: MutableList<State>) {
+            if (state.epsilonTransitions.isNotEmpty()) {
+                for (st in state.epsilonTransitions) {
+                    if (visited.find { it == st } == null) {
+                        visited.add(st);
+                        addNextState(st, nextStates, visited);
+                    }
+                }
+            } else {
+                nextStates.add(state);
+            }
+        }
+
+        fun match(nfa: NFA, word: String): Boolean {
+            var currentStates: MutableList<State> = ArrayList()
+            addNextState(nfa.startState, currentStates, mutableListOf())
+            for (symbol in word) {
+                val nextStatesList: MutableList<State> = mutableListOf()
+                for (st in currentStates) {
+                    val nextState = st.transition[symbol];
+                    if (nextState != null) {
+                        addNextState(nextState, nextStatesList, mutableListOf());
+                    }
+                }
+                currentStates = nextStatesList
+            }
+            return currentStates.find { it.isEndState } != null
+        }
+    }
+}
